ea8f46fec183e476318c1667c941cbc51dccc0ab
RetLight <renzotipula2703@gmail.com>
2024-11-02 03:07:48 -0500

    Routes config

---
+++ routes/user.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require_relative '../controllers/user'
+
+user_controller = UserController.new
+
+post '/iniciar_sesion' do
+  data = JSON.parse(request.body.read)
+  user_controller.iniciar_sesion(data['email'], data['password'], response)
+end
+
+post '/iniciar_sesion_google' do
+  data = JSON.parse(request.body.read)
+  user_controller.iniciar_sesion_google(data['email'], response)
+end
+
+delete '/cerrar_sesion' do
+  user_controller.cerrar_sesion(response)
+end
+
+put '/actualizar_cuenta' do
+  data = JSON.parse(request.body.read)
+  token = request.cookies['myToken']
+  user_controller.actualizar_cuenta(token, data['password'], data['photo'])
+end
+
+get '/obtener_rol' do
+  token = request.cookies['myToken']
+  user_controller.obtener_rol(token)
+end
+
+post '/encontrar_usuario' do
+  data = JSON.parse(request.body.read)
+  user_controller.encontrar_usuario(data['id_ciudadano'])
+end
+
+
+#get '/find_user_token' do
+#  token = request.cookies['myToken']
+#  user_controller.find_user_token(token)
+#end
+
+post '/find_user_by_email' do
+  data = JSON.parse(request.body.read)
+  user_controller.find_user_by_email(data['email'])
+end
+
+post '/reset_password' do
+  data = JSON.parse(request.body.read)
+  user_controller.reset_password(data['email'], data['password'])
+end
