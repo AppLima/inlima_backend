2c031797425c0184c58703393eb0af5456de94de
RetLight <renzotipula2703@gmail.com>
2024-11-03 13:12:25 -0500

    Fix route

--- routes/status.rb
+++ routes/status.rb
@@ -7,5 +7,5 @@ post '/status' do
   data = JSON.parse(request.body.read, symbolize_names: true)
   #token = request.env["HTTP_AUTHORIZATION"]
   #change_controller.record_change(token, data)
-  change_controller.record_change(data)
+  status_controller.record_change(data)
 end
