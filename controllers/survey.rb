73219203166b808c862b0c3e9916b994cb434a43
RetLight <renzotipula2703@gmail.com>
2024-11-03 13:17:04 -0500

    Survey controler + route

---
+++ controllers/survey.rb
@@ -0,0 +1,26 @@
+require json
+require_relative '../DAO/survey'
+
+class SurveyController
+    #def createSurvey(token, data)
+    def createSurvey(data)
+        begin
+            survey = SurveyDAO.create({
+                title: data[:title],
+                description: data[:description],
+                photo: data[:photo],
+                total_results: 0,
+                postives: 0,
+                negatives: 0,
+                start_date: data[:start_date],
+                end_date: data[:end_date],
+                status: 1,
+                district_id: data[:district_id]
+            })
+            { success: true, message: "Encuesta creada con exito", data: survey }.to_json
+        rescue => e
+            puts "Error: #{e.message}"
+            { success: false, message: "Error al crear la encuesta" }.to_json
+        end
+    end
+end
--- routes/survey.rb
+++ routes/survey.rb
@@ -3,7 +3,9 @@ require_relative '../controllers/survey'
 
 survey_controller = SurveyController.new
 
-get '/status' do
-    content_type :json
-    survey_controller.getStatuses
-  end
+post '/create_survey' do
+  data = JSON.parse(request.body.read, symbolize_names: true)
+  #token = request.env["HTTP_AUTHORIZATION"]
+  #change_controller.record_change(token, data)
+  survey_controller.createSurvey(data)
+end
