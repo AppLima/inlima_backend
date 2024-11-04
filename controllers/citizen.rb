b63c7d59e612998c7b5f5ebce95c8d67ac8b4fb6
RetLight <renzotipula2703@gmail.com>
2024-11-04 12:32:59 -0500

    Citizen fix

--- controllers/citizen.rb
+++ controllers/citizen.rb
@@ -2,38 +2,53 @@ require 'json'
 require_relative '../DAO/user'
 require_relative '../DAO/citizen'
 require_relative '../DAO/complaint'
+require_relative '../helpers/notifier'
 
 class CitizenController
-    def register (data)
-        ciudadano = CitizenDAO.find_one_by_email(data[:email]),
-        if ciudadano
-            { success: false, message: "El usuario ya ha sido registrado" }.to_json
-        else
-            usuario = UserDAO.create({
-                email: data[:email],
-                password: data[:password],
-                first_name: data[:first_name],
-                last_name: data[:last_name],
-                photo: '',
-                role_id: 2,
-                gender_id: data[:gender]
-            })
-            if usuario
-                aux = CitizenDAO.create({
-                    dni: data[:dni],
-                    phone_numer: data[:phone_numer],
-                    user_id: usuario[:id],
-                    district_id: data[:district]
-                })
-                if aux
-                    { success: true, message: "Usuario creado con exito"}.to_json
-                else
-                    ##Se tendria que eliminar el usuario creado
-                    { success: false, message: "Error al crear usuario" }.to_json
-                end     
-            else
-                { success: false, message: "Error al crear usuario" }.to_json
-            end
+  def register(data)
+    begin
+      user = UserDAO.find_one_by_email(data[:email])
+      if user
+        return { success: false, message: "El usuario ya ha sido registrado" }.to_json
+      end
+
+      usuario = UserDAO.create({
+        email: data[:email],
+        password: data[:password],
+        first_name: data[:first_name],
+        last_name: data[:last_name],
+        photo: '',
+        role_id: 2,
+        gender_id: data[:gender]
+      })
+
+      if usuario != false
+        begin
+          aux = CitizenDAO.create({
+            dni: data[:dni],
+            phone_number: data[:phone_number],
+            user_id: usuario[:id],
+            district_id: data[:district]
+          })
+
+          if aux
+            Notifier.notify_welcome(usuario[:email], usuario[:first_name])
+            { success: true, message: "Usuario creado con exito" }.to_json
+          else
+            UserDAO.remove(usuario[:id])
+            { success: false, message: "Error al crear usuario" }.to_json
+          end
+        rescue => e
+          UserDAO.remove(usuario[:id])
+          puts "Error al crear ciudadano o enviar notificación: #{e.message}"
+          { success: false, message: "Error al crear ciudadano o enviar notificación" }.to_json
         end
+      else
+        { success: false, message: "Error al crear usuario" }.to_json
+      end
+    rescue => e
+      puts "Error durante el registro de usuario: #{e.message}"
+      { success: false, message: "Error durante el registro de usuario" }.to_json
     end
+  end
 end
--- routes/citizen.rb
+++ routes/citizen.rb
@@ -4,8 +4,7 @@ require_relative '../controllers/citizen'
 citizen_controller = CitizenController.new
 
 post '/register' do
+  content_type :json
   data = JSON.parse(request.body.read, symbolize_names: true)
-  #token = request.env["HTTP_AUTHORIZATION"]
-  #change_controller.record_change(token, data)
   citizen_controller.register(data)
 end
