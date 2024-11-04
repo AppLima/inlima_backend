62a1134cfa6d10f495e6d24aeec343f0d6c4b8c8
RetLight <renzotipula2703@gmail.com>
2024-11-04 12:36:07 -0500

    Response fix

--- controllers/response.rb
+++ controllers/response.rb
@@ -1,18 +1,17 @@
 require 'json'
 require_relative '../DAO/response'
+require_relative '../helpers/auth_helper'
 
 class ResponseController
-    #include AuthHelper
-    #def record_response(token, data)
-    def record_response(data)
+    include AuthHelper
+    def record_response(token, data)
         begin
-            #usuario = verificar_token(token)
-            #ciudadano = CitizenDAO.find_one_by_user_id(usuario[:id])
-            ciudadano = 2
+            usuario = verificar_token(token)
+            ciudadano = CitizenDAO.find_one_by_user_id(usuario[:id])
             
             response = ResponseDAO.create({
                 option: data[:option],
-                citizen_id: ciudadano,#ciudadano[:id],
+                citizen_id: ciudadano[:id],
                 survey_id: data[:survey_id]
             })
 
