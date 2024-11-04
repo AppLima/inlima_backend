98ee8e3fb73e1878f89afa30b9757115e9501efc
RetLight <renzotipula2703@gmail.com>
2024-11-04 12:34:42 -0500

    Survey routes

--- routes/survey.rb
+++ routes/survey.rb
@@ -5,7 +5,5 @@ survey_controller = SurveyController.new
 
 post '/create_survey' do
   data = JSON.parse(request.body.read, symbolize_names: true)
-  #token = request.env["HTTP_AUTHORIZATION"]
-  #change_controller.record_change(token, data)
   survey_controller.createSurvey(data)
 end
