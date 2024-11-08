7b5bc2b7c559a3a63536ef5ba61ce1846c57e83a
RetLight <renzotipula2703@gmail.com>
2024-11-08 07:46:28 -0500

    Response fix

--- routes/response.rb
+++ routes/response.rb
@@ -6,5 +6,5 @@ response_controller = ResponseController.new
 post '/record_response' do
   data = JSON.parse(request.body.read, symbolize_names: true)
   token = request.env["HTTP_AUTHORIZATION"]
-  response_controller.record_change(token, data)
+  response_controller.record_response(token, data)
 end
